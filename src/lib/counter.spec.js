0712616cca7f37f0cb4f180a8617f414e13cf817
Christian Leingang <chrilei2000@gmail.com>
2023-05-17 12:20:46 +0200

    add test case

---
+++ src/lib/counter.spec.js
@@ -0,0 +1,7 @@
+import '@testing-library/jest-dom';
+
+describe('counter component', () => {
+	test('simple test', () => {
+		expect(1).toBe(1);
+	});
+});
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { onMount } from 'svelte';
+	let results = [];
+	onMount(() => {
+		getGiftCards();
+	});
+
+	const getGiftCards = async () => {
+		fetch('https://jsonplaceholder.typicode.com/posts')
+			.then((response) => response.json())
+			.then((json) => {
+				console.log(json);
+				results = json.map((el) => el.title);
+			});
+	};
+</script>
+
+<svelte:head />
+<main class="response">
+	<h1>Posts</h1>
+	{#each results as result}
+		<p>{result}</p>
+	{/each}
+</main>
